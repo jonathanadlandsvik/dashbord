271af3b75cd81d666336dcc2e9f1514430233e0a
Jonathan Ward Ådlandsvik <jonathan.adlandsvik@gmail.com>
2026-08-23 15:18:49 +0200

    Bytt kalender til st.secrets for skydeploy

--- kalender.py
+++ kalender.py
@@ -1,30 +1,24 @@
 import streamlit as st
-import os
 from datetime import datetime, timezone
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
 
 def vis_kalender():
     st.header("📅 Planer i dag")
 
-    SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+    creds = Credentials(
+        token=st.secrets["google"]["access_token"],
+        refresh_token=st.secrets["google"]["refresh_token"],
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=st.secrets["google"]["client_id"],
+        client_secret=st.secrets["google"]["client_secret"],
+        scopes=["https://www.googleapis.com/auth/calendar.readonly"]
+    )
 
-    creds = None
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+    if creds.expired:
+        creds.refresh(Request())
 
     service = build("calendar", "v3", credentials=creds)
 
